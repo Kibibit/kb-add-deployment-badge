a69479774ad4dc165356e55258ce03ea156b83c7
Neil Kalman <Neilkalman@gmail.com>
2021-05-01 12:43:12 +0300

    implementation

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# kb-add-deployment-badge
+<img src="https://kibibit.io/kibibit-assets/badger.png">
+
+# kb-badger-action
 When an Environment is deployed successfully, add a badge to PR body with links to environment
+
+## Inputs
+
+### `who-to-greet`
+
+**Required** The name of the person to greet. Default `"World"`.
+
+## Outputs
+
+### `time`
+
+The time we greeted you.
+
+## Example usage
+
+uses: actions/hello-world-javascript-action@v1.1
+with:
+  who-to-greet: 'Mona the Octocat'
+
+<div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
---
+++ index.js
@@ -0,0 +1,128 @@
+// @ts-check
+const core = require('@actions/core');
+const github = require('@actions/github');
+const _ = require('lodash');
+const urljoin = require('url-join');
+
+const badgeTemplate = _.template(
+  [
+    '[![kb-dep-badge--<%= name %>]',
+    '(https://img.shields.io/badge/<%= left %>-<%= right %>-<%= color %>?logo=<%= logo %>)]',
+    '(<%= url %>)'
+  ].join('')
+);
+
+(async () => {
+  try {
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+    const { context } = github;
+    const badgesData = [];
+    ['badge3', 'badge2', 'badge' ].forEach((name) => {
+      const badge = getBadgeDefinition(name);
+      if (badge) {
+        badgesData.push(badge);
+      }
+    });
+
+    // const { DEPLOYMENT_URL } = process.env;
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+    const DEPLOYMENT_URL = github.context.payload.deployment_status.deployment_url;
+    const owner = context.repo.owner;
+    const repo = context.repo.repo;
+    console.log(DEPLOYMENT_URL);
+    const deploymentData = await getDeploymentData();
+    console.log(deploymentData.prRefs);
+    for (const prRef of deploymentData.prRefs) {
+      if (prRef) {
+        const prId = +prRef.ref.replace('refs/pull/', '').replace('/head', '');
+        console.log({
+          owner,
+          repo,
+          pull_number: prId
+        });
+        const pr = await octokit.pulls.get({
+          owner,
+          repo,
+          pull_number: prId
+        });
+        console.log(pr);
+        let body = pr.data.body;
+
+        badgesData.forEach((badgeData, index) => {
+          const compiledBadge = badgeTemplate(badgeData);
+          if (pr.data.body.includes(badgeData.badgeId)) {
+            // replace badge
+            body = body.replace(badgeData.badgeCatchRegex, compiledBadge);
+          } else {
+            // add badge
+            body = [
+              index === 0 ? '\n\n' : ' ',
+              `${pr.data.body}`
+            ].join('');
+          }
+        });
+
+        await octokit.pulls.update({
+          owner,
+          repo,
+          pull_number: prId,
+          body
+        });
+      }
+    }
+
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+
+    async function getDeploymentData() {
+      const deployment_id = +DEPLOYMENT_URL.replace(/^.*deployments\//, '');
+      const deployment = await octokit.repos.getDeployment({
+        owner,
+        repo,
+        deployment_id
+      });
+      const envUrl = deployment.data.payload.web_url;
+      const commitRef = deployment.data.ref;
+      const refs = await octokit.git.listMatchingRefs({
+        owner,
+        repo,
+        ref: undefined
+      });
+      const prRefs = refs.data.filter((ref) => ref.object.sha === commitRef && ref.ref.startsWith('refs/pull/'));
+
+      return {
+        envUrl,
+        prRefs
+      };
+    }
+
+    function getBadgeDefinition(name) {
+      const left = core.getInput(`${ name }-left`);
+      const right = core.getInput(`${ name }-right`);
+      const color = core.getInput(`${ name }-color`);
+      const urlPath = core.getInput(`${ name }-path`);
+      const logo = core.getInput(`${ name }-logo`);
+      const badgeId = `kb-dep-badge--${ name }`;
+      const badgeCatchRegex = new RegExp(`\[!\[${ badgeId }\]\(.*?\)]\(.*?\)`);
+    
+      if (!left) {
+        return;
+      }
+    
+      return {
+        name,
+        left,
+        right,
+        color,
+        urlPath,
+        logo,
+        badgeCatchRegex
+      };
+    }
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
